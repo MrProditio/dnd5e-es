b80ba1838e9b5a752339b87915a88f4098311c41
MrProditio <arrubicandathulius@hotmail.com>
2025-11-09 12:30:10 +0100

    5.4.0
    Segregación de las traducciones del sistema y los compendios SRD, de los Manuales de D&D

--- babele-register.js
+++ babele-register.js
@@ -1,40 +1,39 @@
 import {Converters} from "../babele/script/converters.js";
 
 Hooks.once('init', () => {
-  if (typeof Babele !== 'undefined') {
+	if(typeof Babele !== 'undefined') {
 
-    console.log('***********************');
-    console.log('*** Babele DnD5e ES ***');
-    console.log('***********************');
+		console.log('***********************');
+		console.log('*** Babele DnD5e ES ***');
+		console.log('***********************');
 
-    // Registro de módulo y carpeta de traducciones
-    Babele.get().register({
-      module: 'dnd5e-es',
-      lang: 'es',
-      dir: 'compendium'
-    });
+		Babele.get().register({
+			module: 'dnd5e-es',
+			lang: 'es',
+			dir: 'compendium'
+		});
 
-    // Converter existente
-    Babele.get().registerConverters({
-      'dnd5ePages': (pages, translations) => {
-        pages = Converters._pages(pages, translations);
+		Babele.get().registerConverters({
+			'dnd5ePages': (pages, translations) => {
+				pages = Converters._pages(pages, translations);
+	
+				return pages.map(data => {
+					if (!translations) {
+						return data;
+					}
 
-        return pages.map(data => {
-          if (!translations) return data;
-
-          const translation = translations[data._id] || translations[data.name];
-          if (!translation) return data;
-
-          return foundry.utils.mergeObject(data, {
-            system: {
-              tooltip: translation.tooltip ?? data.system.tooltip
-            }
-          });
-        });
-      },
-
-      // NUEVO converter para merge seguro de items/efectos/actividades
-      'safeMergeEntity': Converters.safeMergeEntity
-    });
-  }
+					const translation = translations[data._id] || translations[data.name];
+					
+					if (!translation) {
+						return data;
+					}
+					return foundry.utils.mergeObject(data, {
+						system: {
+							tooltip: translation.tooltip ?? data.system.tooltip
+						}
+					});
+				});
+			}
+		});
+	}
 });
